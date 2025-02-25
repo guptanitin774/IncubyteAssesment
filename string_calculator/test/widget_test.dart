1c52c683a41dc2f574d95457804cec07c674dad5
nitinguptahipl <122363059+nitinguptahipl@users.noreply.github.com>
2025-02-25 19:57:08 +0530

    Implemented String Calculator with TDD

--- string_calculator/test/widget_test.dart
+++ string_calculator/test/widget_test.dart
@@ -1,30 +1,14 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:string_calculator/main.dart';
+import 'package:string_calculator/string_calculator.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  print(StringCalculator.add('')); // Output: 0
+  print(StringCalculator.add('1')); // Output: 1
+  print(StringCalculator.add('1,2')); // Output: 3
+  print(StringCalculator.add('1\n2,3')); // Output: 6
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  try {
+    print(StringCalculator.add('-1,2,-3,4'));
+  } catch (e) {
+    print(e); // Output: FormatException: negative numbers not allowed -1, -3
+  }
 }
